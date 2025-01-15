add72107d84bb03f2fd1abbc0dbbf2eed0fac765
thangnd-zenai <thangnd.zenai@gmail.com>
2025-01-15 07:57:55 +0000

    update: setup and readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# SAM HQ
+Source: https://github.com/SysCV/sam-hq
+# Install
+```bash
+pip install git+https://github.com/ZenAI-Vietnam/sam-hq.git
+```
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sam_hq",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "torch>=1.7.0",
+        "torchvision",
+        "numpy",
+        "opencv-python",
+        "timm",
+        "segment-anything"
+    ],
+    author="HQ-SAM team",
+    description="High Quality Segment Anything Model",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SysCV/sam-hq",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
